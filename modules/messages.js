5dd89d767603b1097fd789406a52cdd02c09b591
Andrei Pulsha <ap.ttnby@gmail.com>
2016-04-01 18:19:38 +0300

    yandex

--- modules/messages.js
+++ modules/messages.js
@@ -1,10 +1,3 @@
-// This is essentially bulk require
-/*var req = require.context('../locales', true, /\.json.*$/); 
-req.keys().forEach(function (file) {  
-  var locale = file.replace('./', '').replace('.json', '');
-  exports[locale] = req(file);
-});*/ 
-
 var exports = {};
 exports.supportLanguages = [ 
 	{
